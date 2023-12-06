47bd787ceca38c5c612def2ccb4535be5e24ea23
Serg Stetsuk <scoute.me@gmail.com>
2023-12-06 18:32:55 +0400

    Стартовая точка, где реализовано проигрывание аудио-файла и зачатки TAG

---
+++ mainwindow.cpp
@@ -0,0 +1,181 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    M_Player = new QMediaPlayer();
+    ui->pushButton_Play->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    ui->pushButton_Pause->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+
+    ui->hSlider_VolumeControl->setMinimum(1);
+    ui->hSlider_VolumeControl->setMaximum(100);
+    ui->hSlider_VolumeControl->setValue(10);
+
+    // изначальная громкость 10%
+    M_Player->setVolume(10);
+
+    connect(M_Player,&QMediaPlayer::durationChanged,this,&MainWindow::durationChanged);
+    connect(M_Player,&QMediaPlayer::positionChanged,this,&MainWindow::positionChanged);
+
+    ui->hSlider_AudioFileDuration->setRange(0,M_Player->duration() / 1000);
+    ui->hSlider_TagTimeBegin->setRange(0,M_Player->duration() / 1000);
+
+    //ui->hSlider_TagTimeBegin->setStyleSheet("QSlider::groove:horizontal {background-color:red;}");
+    //ui->hSlider_TagTimeBegin->setStyleSheet("QSlider::groove:horizontal { border: 1px solid #999999; height: 8px; background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #B1B1B1, stop:1 #c4c4c4);  margin: 2px 0; }");
+
+    //ui->hSlider_TagTimeBegin->setMaximumWidth(100);
+    //ui->hSlider_TagTimeBegin->setMinimumWidth(200);
+}
+
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::updateduration(qint64 duration)
+{
+    //QString timestr;
+    if (duration || Mduration)
+    {
+        QTime CurrentTime((duration / 3600) % 60, (duration / 60) % 60, duration % 60, duration * 1000 % 1000);
+        QTime totalTime((Mduration / 3600) % 60, (Mduration / 60) % 60, Mduration % 60, Mduration * 1000 % 1000);
+        QString format = "mm:ss";
+        //QString format = "mm:ss.z";
+
+        // если длительность аудио больше часа, ставим "hh:mm:ss" вместо "mm:ss"
+        if (Mduration > 3600)
+        {
+            format = "hh:mm:ss";
+        }
+
+        ui->label_CurrTime->setText(CurrentTime.toString(format)); // + " / " + totalTime.toString(format));
+        ui->label_Total_Time->setText(totalTime.toString(format));
+    }
+}
+
+
+void MainWindow::durationChanged(qint64 duration)
+{
+    Mduration = duration / 1000;
+    ui->hSlider_AudioFileDuration->setMaximum(Mduration);
+    ui->hSlider_TagTimeBegin->setMaximum(Mduration);
+    ui->hSlider_TagTimeEnd->setMaximum(Mduration);
+}
+
+
+void MainWindow::positionChanged(qint64 progress)
+{
+    if (!ui->hSlider_AudioFileDuration->isSliderDown())
+    {
+      ui->hSlider_AudioFileDuration->setValue(progress / 1000);
+    }
+    updateduration(progress / 1000);
+}
+
+
+void MainWindow::on_actionOpen_File_triggered()
+{
+    QString FileName = QFileDialog::getOpenFileName(this,tr("Select Audio File"),tr("MP3 Files (*.MP3)"));
+    M_Player->setMedia(QUrl("file://" + FileName));
+
+    QFileInfo File(FileName);
+    ui->lbl_Value_File_Name->setText(File.fileName());
+
+    //M_Player->play();
+    MainWindow::on_pushButton_Play_clicked();
+}
+
+
+void MainWindow::on_pushButton_Play_clicked()
+{
+    M_Player->play();
+
+}
+
+
+void MainWindow::on_pushButton_Pause_clicked()
+{
+    M_Player->pause();
+}
+
+
+void MainWindow::on_hSlider_VolumeControl_valueChanged(int value)
+{
+    M_Player->setVolume(value);
+}
+
+
+void MainWindow::on_hSlider_AudioFileDuration_sliderMoved(int position)
+{
+    // M_Player->setPosition(position * 1000);
+    //movedposition=position;
+}
+
+
+void MainWindow::on_hSlider_AudioFileDuration_sliderReleased()
+{
+    //M_Player->setPosition(movedposition * 1000);
+    M_Player->setPosition(ui->hSlider_AudioFileDuration->value()*1000);
+
+    //ui->hSlider_TagTimeBegin->setMaximumWidth(movedposition * 10);
+    //ui->hSlider_TagTimeBegin->setMinimumWidth(movedposition * 10);
+}
+
+
+void MainWindow::on_hSlider_TagTimeBegin_sliderMoved(int position)
+{
+
+}
+
+
+void MainWindow::on_hSlider_TagTimeBegin_valueChanged(int value)
+{
+    //QTime CurrentTime((duration / 3600) % 60, (duration / 60) % 60, duration % 60, duration * 1000 % 1000);
+    //QTime totalTime((Mduration / 3600) % 60, (Mduration / 60) % 60, Mduration % 60, Mduration * 1000 % 1000);
+    //QString format = "mm:ss";
+    //ui->label_TAGBeginTime->setText(CurrentTime.toString(format));
+    //ui->label_TAGBeginTime->setText(QString::number( value ).toString("mm:ss"));
+}
+
+
+void MainWindow::on_hSlider_TagTimeBegin_sliderReleased()
+{
+
+}
+
+
+void MainWindow::on_hSlider_TagTimeEnd_sliderMoved(int position)
+{
+
+}
+
+
+void MainWindow::on_hSlider_TagTimeEnd_sliderReleased()
+{
+
+}
+
+
+void MainWindow::on_pushButton_TAGSetBeginTime_clicked()
+{
+    ui->label_TAGBeginTime->setText(ui->label_CurrTime->text());
+    ui->hSlider_TagTimeBegin->setValue(ui->hSlider_AudioFileDuration->value());
+}
+
+
+void MainWindow::on_pushButton_TAGSetEndTime_clicked()
+{
+    ui->label_TAGEndTime->setText(ui->label_CurrTime->text());
+    ui->hSlider_TagTimeEnd->setValue(Mduration - ui->hSlider_AudioFileDuration->value());
+}
+
+
+
+
+
---
+++ mainwindow.h
@@ -0,0 +1,53 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <QtMultimedia>
+#include <QFileDialog>
+#include <QStyle>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
+
+private slots:
+  void durationChanged(qint64 duration);
+  void positionChanged(qint64 progress);
+
+  void on_actionOpen_File_triggered();
+
+  void on_pushButton_Play_clicked();
+  void on_pushButton_Pause_clicked();
+
+  void on_hSlider_VolumeControl_valueChanged(int value);
+  void on_hSlider_AudioFileDuration_sliderMoved(int position);
+  void on_hSlider_AudioFileDuration_sliderReleased();
+
+  void on_hSlider_TagTimeBegin_sliderMoved(int position);
+  void on_hSlider_TagTimeBegin_sliderReleased();
+  void on_hSlider_TagTimeEnd_sliderMoved(int position);
+  void on_hSlider_TagTimeEnd_sliderReleased();
+
+  void on_pushButton_TAGSetBeginTime_clicked();
+  void on_pushButton_TAGSetEndTime_clicked();
+
+  void on_hSlider_TagTimeBegin_valueChanged(int value);
+
+private:
+  void updateduration(qint64 duration);
+  Ui::MainWindow *ui;
+  QMediaPlayer *M_Player;
+  qint64 Mduration;
+  int movedposition;
+};
+#endif // MAINWINDOW_H
