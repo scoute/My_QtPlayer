9f3187de09a914a8441cc6552899dac53b7ad4a8
Serg Stetsuk <scoute.me@gmail.com>
2023-12-16 17:42:51 +0400

    Рабочая версия ползунков с миллисекундами.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,8 +22,12 @@ MainWindow::MainWindow(QWidget *parent)
     connect(M_Player,&QMediaPlayer::durationChanged,this,&MainWindow::durationChanged);
     connect(M_Player,&QMediaPlayer::positionChanged,this,&MainWindow::positionChanged);
 
+    /*
     ui->hSlider_AudioFileDuration->setRange(0,M_Player->duration() / 1000);
     ui->hSlider_TagTimeBegin->setRange(0,M_Player->duration() / 1000);
+    */
+    ui->hSlider_AudioFileDuration->setRange(0,M_Player->duration());
+    ui->hSlider_TagTimeBegin->setRange(0,M_Player->duration());
 
     //ui->hSlider_TagTimeBegin->setStyleSheet("QSlider::groove:horizontal {background-color:red;}");
     //ui->hSlider_TagTimeBegin->setStyleSheet("QSlider::groove:horizontal { border: 1px solid #999999; height: 8px; background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #B1B1B1, stop:1 #c4c4c4);  margin: 2px 0; }");
@@ -44,8 +48,16 @@ void MainWindow::updateduration(qint64 duration)
     //QString timestr;
     if (duration || Mduration)
     {
+        /*
         QTime CurrentTime((duration / 3600) % 60, (duration / 60) % 60, duration % 60, duration * 1000 % 1000);
         QTime totalTime((Mduration / 3600) % 60, (Mduration / 60) % 60, Mduration % 60, Mduration * 1000 % 1000);
+        */
+
+
+        QTime CurrentTime((duration / 3600 / 1000) % 60, (duration / 60 / 1000) % 60, (duration / 1000) % 60, duration % 1000);
+        QTime totalTime((Mduration / 3600 / 1000) % 60, (Mduration / 60 / 1000) % 60, (Mduration / 1000) % 60, Mduration % 1000);
+
+
         //QString format = "mm:ss";
         QString format = "mm:ss.zzz";
 
@@ -64,7 +76,8 @@ void MainWindow::updateduration(qint64 duration)
 
 void MainWindow::durationChanged(qint64 duration)
 {
-    Mduration = duration / 1000;
+    //Mduration = duration / 1000;
+    Mduration = duration;
     ui->hSlider_AudioFileDuration->setMaximum(Mduration);
     ui->hSlider_TagTimeBegin->setMaximum(Mduration);
     ui->hSlider_TagTimeEnd->setMaximum(Mduration);
@@ -78,9 +91,11 @@ void MainWindow::positionChanged(qint64 progress)
 {
     if (!ui->hSlider_AudioFileDuration->isSliderDown())
     {
-      ui->hSlider_AudioFileDuration->setValue(progress / 1000);
+      //ui->hSlider_AudioFileDuration->setValue(progress / 1000);
+        ui->hSlider_AudioFileDuration->setValue(progress);
     }
-    updateduration(progress / 1000);
+    //updateduration(progress / 1000);
+    updateduration(progress);
 }
 
 
@@ -118,16 +133,28 @@ void MainWindow::on_hSlider_VolumeControl_valueChanged(int value)
 
 
 void MainWindow::on_hSlider_AudioFileDuration_sliderMoved(int position)
-{
-    // M_Player->setPosition(position * 1000);
-    //movedposition=position;
+{ 
+    QTime CurrentTime((position / 3600 / 1000) % 60, (position / 60 / 1000) % 60, (position / 1000) % 60, position % 1000);
+
+    //QString format = "mm:ss";
+    QString format = "mm:ss.zzz";
+
+    // если длительность аудио больше часа, ставим "hh:mm:ss" вместо "mm:ss"
+    if (Mduration > 3600)
+    {
+        //format = "hh:mm:ss";
+        format = "hh:mm:ss.zzz";
+    }
+
+    ui->label_CurrTime->setText(CurrentTime.toString(format));
+    //ui->label_CurrTime->setText(position.toString("mm:ss.zzz"));
 }
 
 
 void MainWindow::on_hSlider_AudioFileDuration_sliderReleased()
 {
-    //M_Player->setPosition(movedposition * 1000);
-    M_Player->setPosition(ui->hSlider_AudioFileDuration->value()*1000);
+    //M_Player->setPosition(ui->hSlider_AudioFileDuration->value()*1000);
+    M_Player->setPosition(ui->hSlider_AudioFileDuration->value());
 
     //ui->hSlider_TagTimeBegin->setMaximumWidth(movedposition * 10);
     //ui->hSlider_TagTimeBegin->setMinimumWidth(movedposition * 10);
